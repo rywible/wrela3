e743084568874bbc8a376b9dd38e08d9db2bc436
Ryan Wible <ryanwible@Ryans-MacBook-Air.local>
2026-05-16 23:54:02 -0600

    feat: enumerate multiple pci ecam windows -Codex Automated

--- compiler/sem/pci_ecam_contract_test.go
+++ compiler/sem/pci_ecam_contract_test.go
@@ -12,7 +12,6 @@ func TestPciEcamEnumerationSourceContract(t *testing.T) {
 		"device << 15",
 		"function << 12",
 		"offset & 0x0FFC",
-		"while bus <= self.window0.end_bus",
 		"while device < 32",
 		"while function < 8",
 		"vendor_device & 0xFFFF",
@@ -20,6 +19,11 @@ func TestPciEcamEnumerationSourceContract(t *testing.T) {
 		"self.panic.fail(code = 0xAC060013)",
 		"self.panic.fail(code = 0xAC060012)",
 		"self.panic.fail(code = 0xAC060010)",
+		"window1: PcieEcamWindow",
+		"window2: PcieEcamWindow",
+		"window3: PcieEcamWindow",
+		"self.panic.fail(code = 0xAC060015)",
+		"devices.scan_window(window = self.at(index = index))",
 	}
 	for _, needle := range required {
 		if !strings.Contains(source, needle) {
